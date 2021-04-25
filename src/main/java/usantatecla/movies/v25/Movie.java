e0cbc80c630f33c857c571a17946008369ca3e43
mbendohhoulaouini <mohammed.bendohhou.laouini@alumnos.upm.es>
2021-04-25 17:31:03 +0200

    v25: Colapsar la clase Price y sus derivadas como parte de la responsabilidad de Movie y sus nuevas derivadas.

---
+++ src/main/java/usantatecla/movies/v25/Movie.java
@@ -0,0 +1,23 @@
+package usantatecla.movies.v25;
+
+public abstract class Movie {
+
+    private static final int DAYS_RENTED_THRESHOLD = 0;
+    private static final int FREQUENT_RENTER_POINTS = 1;
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return Movie.FREQUENT_RENTER_POINTS;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
